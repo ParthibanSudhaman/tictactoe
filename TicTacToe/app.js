dacc2043579c3df515020452b86a6a228401a965
Parthiban.Sudhaman <psudhaman@hotmail.com>
2015-03-06 11:39:44 +0530

    draft still

--- TicTacToe/app.js
+++ TicTacToe/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿(function () {
+    'use strict';
+    var app = angular.module('', ['ngRoute', 'ngAnimate']);
+
+    app.value('signalRServer', 'http://localhost:64316');
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when('/',
+        {
+            controller: '',
+            templateUrl: ''
+        })
+        .otherwise({ redirectTo: '/' });
+    });
+}());
